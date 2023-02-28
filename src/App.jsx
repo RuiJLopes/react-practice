5ce2d483277906df4029a4b44cf2b1a55ef69162
Rui Lopes <ruilopes@Ruis-Air.Home>
2023-02-28 18:07:32 +0000

    Create modal and backdrop components. Add the desired funcitons

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Learn Muay Thai" />
+      <Todo text="Explore the world" />
     </div>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,33 @@
-function Todo() {
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
+function Todo(props) {
+  const [showModal, setShowModal] = useState(false);
+  //Nested function
+  function deleteHandler() {
+    //Render a modal and overlay
+    setShowModal(true);
+  }
+
+  function closeModalHandler() {
+    setShowModal(false);
+  }
   return (
     <>
       <div className="card">
-        <h2>TITLE</h2>
+        <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn">Delete</button>
+          <button className="btn" onClick={deleteHandler}>
+            Delete
+          </button>
         </div>
+        {showModal && (
+          <>
+            <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+            <Backdrop onCancel={closeModalHandler} />
+          </>
+        )}
       </div>
     </>
   );
